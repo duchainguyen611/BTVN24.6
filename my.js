caa2fd94f9af27d3c8596ef46cc294f261de9fe6
Scorpio <83647081+duchainguyen611@users.noreply.github.com>
2021-06-26 16:24:25 +0700

    Update my.js

--- my.js
+++ my.js
@@ -1,3 +1,6 @@
+function sumWithout1Number(sum,arr){
+    return sum - arr;
+}
 
 function sumMinMax(arr){
     let arr2 = [];
@@ -6,11 +9,11 @@ function sumMinMax(arr){
     for(let i=0;i<5;i++){
         sum+=arr[i];
     }
-    let sum0 = sum - arr[0];
-    let sum1 = sum - arr[1];
-    let sum2 = sum - arr[2];
-    let sum3 = sum - arr[3];
-    let sum4 = sum - arr[4];
+    let sum0 = sumWithout1Number(sum,arr[0]);
+    let sum1 = sumWithout1Number(sum,arr[1]);
+    let sum2 = sumWithout1Number(sum,arr[2]);
+    let sum3 = sumWithout1Number(sum,arr[3]);
+    let sum4 = sumWithout1Number(sum,arr[4]);
     arr2.push(sum0,sum1,sum2,sum3,sum4);
     let min = arr2[0];
     let max = arr2[0];
